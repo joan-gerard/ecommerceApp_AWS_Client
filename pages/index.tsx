8507fe1718203d0660618d5cd0065d538a9e1efb
Joan Gerard <joan.gerard@outlook.com>
2023-02-24 15:16:03 +0100

    define all components needed for project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { Product, HeroBanner, FooterBanner } from "@/components";
 
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>
+      <div className="products-container">
         {["Product 1", "Product 2", "Product 3"].map((product) => product)}
       </div>
-      Footer
+      <FooterBanner />
     </>
   );
 };
