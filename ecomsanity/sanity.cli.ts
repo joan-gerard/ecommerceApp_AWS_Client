b5baa289ffd88742219cc0276ce0a425905fb31d
Joan Gerard <joan.gerard@outlook.com>
2023-03-13 20:19:23 +0100

    attempt to fix deploy error

--- ecomsanity/sanity.cli.ts
+++ ecomsanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from '@sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '58nl9cc2',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
