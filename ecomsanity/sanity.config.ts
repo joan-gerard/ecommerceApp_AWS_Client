1acfef2b936141e653f2c7bd732659894fd2a950
Joan Gerard <joan.gerard@outlook.com>
2023-03-13 21:17:12 +0100

    attempt to fix deploy error

--- ecomsanity/sanity.config.ts
+++ ecomsanity/sanity.config.ts
@@ -3,10 +3,6 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
-// const {defineConfig} = require('sanity')
-// const {deskTool} = require('sanity/desk')
-// const {visionTool} = require('@sanity/vision')
-
 export default defineConfig({
   name: 'default',
   title: 'ecom_sanity',
