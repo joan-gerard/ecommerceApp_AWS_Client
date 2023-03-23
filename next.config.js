507b6705edde2753e062564f15fb6dc1a0e60578
Joan Gerard <joan.gerard@outlook.com>
2023-03-23 19:06:35 +0100

    implement logic to write to the Dynamo Order DB upon successful Stripe payment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    stripeKey: process.env.STRIPE_SECRET_KEY
+  }
 }
 
 module.exports = nextConfig
--- pages/[sucess].tsx
+++ pages/[sucess].tsx
@@ -1,15 +1,54 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { BsBagCheckFill } from "react-icons/bs";
+import { Auth } from "aws-amplify";
+import Axios, { Method, AxiosRequestConfig } from "axios";
 
 import { useStateContext } from "@/context/stateContext";
-import { runFireworks } from "@/lib/utils";
+import { handlePlaceOrder, handleRemoveStorageItems, runFireworks } from "@/lib/utils";
+
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const Success = () => {
-  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+  console.log("SUCCESS PAYMENT");
+  const { setCartItems, setTotalPrice, setTotalQuantities, cartItems } =
+    useStateContext();
+
+  const [ordedPlaced, setOrdedPlaced] = useState([]);
 
   useEffect(() => {
-    localStorage.clear();
+    const queryString = window.location.search;
+    console.log({ queryString });
+
+    const urlParams = new URLSearchParams(queryString);
+    console.log({ urlParams });
+
+    const sessionID = urlParams.get("session_id");
+    console.log({ sessionID });
+
+    console.log("KEY", process.env.STRIPE_SECRET_KEY);
+    const headers = {
+      Authorization: `Bearer ${process.env.stripeKey}`,
+    };
+    const requestConfig: AxiosRequestConfig = {
+      headers,
+      method: "GET",
+      url: `https://api.stripe.com/v1/checkout/sessions/${sessionID}/line_items?limit=5`,
+    };
+
+    const getData = async () => {
+      const axiosRes = await Axios.request(requestConfig).then(
+        (res) => res.data.data
+      );
+
+      console.log({ axiosRes });
+      handlePlaceOrder(axiosRes)
+    };
+
+    getData();
+
+    // Reset data
+    handleRemoveStorageItems();
     setCartItems([]);
     setTotalPrice(0);
     setTotalQuantities(0);
